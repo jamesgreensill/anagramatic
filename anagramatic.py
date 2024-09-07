88bd66944e10d293dc5a6426ea287d1fcf432906
thejamesgreensill <thejamesgreensill@gmail.com>
2024-09-07 12:02:00 +1000

    updated to write a file with all the words that are not anagrams

--- anagramatic.py
+++ anagramatic.py
@@ -2,7 +2,9 @@ if __name__ == "__main__":
     with open("data/words.txt", "r") as file:
         lines = file.readlines()
         anagrams = {}
+        all_anagram_words = set()  # To store all words that are part of anagrams
 
+        # Process each line to sort alphabetically and group anagrams
         for line in lines:
             # Remove new lines and spaces
             clean_line = line.strip()
@@ -15,12 +17,16 @@ if __name__ == "__main__":
         filtered_anagrams = {key: value for key,
                              value in anagrams.items() if len(value) > 1}
 
+        # Collect all words that are part of anagrams
+        for words in filtered_anagrams.values():
+            all_anagram_words.update(words)
+
         # Get the length of unique anagrams
         unique_anagrams = len(filtered_anagrams)
 
         # Get the length of all anagrams
-        total_words_in_groups = sum(
-            len(words) for words in filtered_anagrams.values())
+        total_words_in_groups = sum(len(words)
+                                    for words in filtered_anagrams.values())
 
         # Get the length of the initial data-set
         total_words_in_file = len(lines)
@@ -37,11 +43,11 @@ if __name__ == "__main__":
         sorted_anagrams = sorted(filtered_anagrams.items(
         ), key=lambda item: len(item[1]), reverse=True)
 
-        # Output to file.
+        # Write the anagrams information to file
         with open("anagrams.txt", "w") as output_file:
             output_file.write(f"Total Anagram Groups: {unique_anagrams}\n")
-            output_file.write(f"Total Unique Anagrams: 
-                              {total_words_in_groups}\n")
+            output_file.write(f"Total Unique Anagrams: {
+                              total_words_in_groups}\n")
             output_file.write(
                 f"Total Words in Original Data-set: {total_words_in_file}\n")
             output_file.write(
@@ -51,3 +57,12 @@ if __name__ == "__main__":
             for key, value in sorted_anagrams:
                 count = len(value)
                 output_file.write(f"{count}: {', '.join(value)}\n")
+
+        # Filter out anagram words from the original list
+        non_anagram_words = [
+            line.strip() for line in lines if line.strip() not in all_anagram_words]
+
+        # Write non-anagram words to a new file
+        with open("non_anagrams.txt", "w") as non_anagrams_file:
+            non_anagrams_file.write("All words that are not anagrams.\n\n")
+            non_anagrams_file.write("\n".join(non_anagram_words))
